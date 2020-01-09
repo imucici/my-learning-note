17d5a14ecacd8a3eff7d110ba4aded2cf602b1b5
imucici <52574328+imucici@users.noreply.github.com>
2020-01-09 10:55:18 +0800

    Update and rename 852. Peak Index in a Mountain Array.ipynb to 852. Peak Index in a Mountain Array.py

---
+++ LeetCode/week12/852. Peak Index in a Mountain Array.py
@@ -0,0 +1,7 @@
+# 題目:
+
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        for i in range(1,len(A)-1):
+            if A[i] > A[i-1] and A[i] > A[i+1] :
+                return i
