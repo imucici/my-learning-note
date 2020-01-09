dace38e9695063240100a78b76b2b66da808b6e8
imucici <52574328+imucici@users.noreply.github.com>
2020-01-09 11:20:28 +0800

    Update and rename 217. Contains Duplicate.ipynb to 217. Contains Duplicate.py

---
+++ LeetCode/week4/217. Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        a = set(nums)
+        if len(a) == len(nums):
+            return False
+        else:
+            return True
+    
